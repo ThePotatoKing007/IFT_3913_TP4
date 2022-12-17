8bc8af19a49b7d1a13a97aed1bd76553a9773a9f
ThePotatoKing007 <44982901+ThePotatoKing007@users.noreply.github.com>
2022-12-16 20:03:04 -0500

    Create CurrencyConvertorTest.java

---
+++ ExpanseManagerCopy/src/test/java/ua/karatnyk/impl/CurrencyConvertorTest.java
@@ -0,0 +1,18 @@
+package ua.karatnyk.impl;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class CurrencyConvertorTest {
+
+	
+		private Double prix;
+		private String deviseDeBase;
+		private String deviseDArriver;
+		
+}
