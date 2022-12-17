b8bcb24083f6d36861d92eaec06b76d90837a8f2
ThePotatoKing007 <44982901+ThePotatoKing007@users.noreply.github.com>
2022-12-16 21:29:07 -0500

    Tests Boite noire
    Tout les test de à boite noire

--- ExpanseManagerCopy/src/test/java/ua/karatnyk/impl/CurrencyConvertorTest.java
+++ ExpanseManagerCopy/src/test/java/ua/karatnyk/impl/CurrencyConvertorTest.java
@@ -3,7 +3,9 @@ package ua.karatnyk.impl;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
+import java.text.ParseException;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -11,8 +13,124 @@ import static org.junit.jupiter.api.Assertions.*;
 class CurrencyConvertorTest {
 
 	
-		private Double prix;
-		private String deviseDeBase;
-		private String deviseDArriver;
+		private Double amount;
+		private String from;
+		private String to;
+		private static CurrencyConversion conversion;
+		public Class<Exception> exceptionClass = Exception.class;
+		
+		
+		@BeforeAll
+		public static void setup() {
+			
+			conversion = new CurrencyConversion();
+		}
+		
+		@BeforeEach
+		public void init() {
+			
+			amount = 1.00; 
+			from = "CAD";
+			to = "USD";
+			
+		}
+		
+		@Test
+	    public void isNotConvertingFrom() {
+	        String wrongFrom = "ETH";
+	        String expectedOutcome = "Not correct format currency";
+
+	        assertThrows(exceptionClass,
+	                () -> CurrencyConvertor.convert(amount, wrongFrom, to, conversion), expectedOutcome);
+	    }
+		
+		@Test
+	    public void isNotConvertingTo() {
+	        String wrongTo = "ETH";
+	        String expectedOutcome = "Not correct format currency";
+
+	        assertThrows(exceptionClass,
+	                () -> CurrencyConvertor.convert(amount, from, wrongTo, conversion), expectedOutcome);
+	    }
+		
+		@Test
+		public void isConvertingValidCurrency() throws ParseException {
+			
+			double curencyFrom = conversion.getRates().get(from);
+			double curencyTo = conversion.getRates().get(to);
+			double results = amount*(curencyFrom/curencyTo);
+			
+			assertTrue(results == CurrencyConvertor.convert(amount, from, to, conversion)); // to find if it's a double
+		}
+		
+		@Test
+	    void isSupportingSameCurrency() throws ParseException {
+	        // USD, CAD, GBP, EUR, CHF, INR, AUD
+	        double valeurParDefault = 10.00;
+
+	        double USDaUSD = CurrencyConvertor.convert(valeurParDefault,"USD","USD", conversion);
+	        double CADaCAD = CurrencyConvertor.convert(valeurParDefault,"CAD","CAD", conversion);
+	        double GBPaGBP = CurrencyConvertor.convert(valeurParDefault,"GBP","GBP", conversion);
+	        double EURaEUR = CurrencyConvertor.convert(valeurParDefault,"EUR","EUR", conversion);
+	        double CHFaCHF = CurrencyConvertor.convert(valeurParDefault,"CHF","CHF", conversion);
+	        double INRaINR = CurrencyConvertor.convert(valeurParDefault,"INR","INR", conversion);
+	        double AUDaAUD = CurrencyConvertor.convert(valeurParDefault,"AUD","AUD", conversion);
+
+	        assertNotNull(USDaUSD, "Devrait supporter USD");
+	        assertNotNull(CADaCAD, "Devrait supporter CAD");
+	        assertNotNull(GBPaGBP, "Devrait supporter GBP");
+	        assertNotNull(EURaEUR, "Devrait supporter EUR");
+	        assertNotNull(CHFaCHF, "Devrait supporter CHF");
+	        assertNotNull(INRaINR, "Devrait supporter INR");
+	        assertNotNull(AUDaAUD, "Devrait supporter AUD");
+	        
+
+	    }
+		
+		@Test
+	    public void isNotConvertingAmountMoreThan10k() {
+	        Double superiorAmount = 99999.00d;
+	        String expectedOutcome = "Amount not in the possible range";
+	        
+	        assertThrows(exceptionClass,
+	                () -> CurrencyConvertor.convert(superiorAmount, from, to, conversion), expectedOutcome);
+	    }
+		
+		@Test
+	    public void isNotConvertingAmountLessThan0() {
+	        Double inferiorAmount = -5.00d;
+	        String expectedOutcome = "Amount not in the possible range";
+	        
+	        assertThrows(exceptionClass,
+	        		() -> CurrencyConvertor.convert(inferiorAmount, from, to, conversion), expectedOutcome);
+	    }
+		
+		@Test
+	    public void isConvertingAmount10k() {
+	        Double goodAmount = 10000.00d;
+	        String expectedOutcome = "Amount in the possible range";
+	        
+	        assertThrows(exceptionClass,
+	        		() -> CurrencyConvertor.convert(goodAmount, from, to, conversion), expectedOutcome);
+	    }
+		
+		@Test
+	    public void isConvertingAmount500() {
+	        Double goodAmount = 500.00d;
+	        String expectedOutcome = "Amount in the possible range";
+	        
+	        assertThrows(exceptionClass,
+	        		() -> CurrencyConvertor.convert(goodAmount, from, to, conversion), expectedOutcome);
+	    }
+		
+		@Test
+	    public void isConvertingAmount0() {
+	        Double goodAmount = 0.00d;
+	        String expectedOutcome = "Amount in the possible range";
+	        
+	        assertThrows(exceptionClass,
+	        		() -> CurrencyConvertor.convert(goodAmount, from, to, conversion), expectedOutcome);
+	    }
+		
 		
 }
